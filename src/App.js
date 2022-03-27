135c383c2550c628dde38561fbce8ae2ab4cf56b
Hemant <hemantrastogi17@gmail.com>
2022-03-28 04:16:11 +0530

    Defined components and redux store+ coins fetching

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+import { Navbar, Homepage, Exchanges, CryptoDetails, Cryptocurrencies, News } from './components';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div className='app'>
+            <div className="navbar">
+                <Navbar />
+            </div>
+            <div className="main">
+                <Layout>
+                    <div className="routes">
+                        <Routes>
+                            <Route exact path="/" element={<Homepage />} />
+                            <Route exact path="/exchanges" element={<Exchanges />} />
+                            <Route exact path="/cryptocurrencies" element={<Cryptocurrencies />} />
+                            <Route exact path="/crypto/:coinId" element={<CryptoDetails />} />
+                            <Route exact path="/news" element={<News />} />
+                        </Routes>
 
-export default App;
+                    </div>
+                </Layout>
+
+                <div className="footer">
+                    <Typography.Title level={4} style={{color: 'white', textAlign:'center'}}>
+                        Coinella | All Rights Reserved ©
+                    </Typography.Title>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default App
